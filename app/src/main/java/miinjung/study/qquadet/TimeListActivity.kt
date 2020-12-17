1f54cf5a001ab9ff2c15cda13d3122ebf657d078
hyungraelee <timgg12345@gmail.com>
2020-12-17 15:49:45 +0900

    init_timelist

---
+++ app/src/main/java/miinjung/study/qquadet/TimeListActivity.kt
@@ -0,0 +1,11 @@
+package miinjung.study.qquadet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimeListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_list)
+    }
+}
