f4faed24780b5d933c7ffc68c892abb4f86ed45f
hyungraelee <timgg12345@gmail.com>
2020-12-18 12:34:45 +0900

    fix number

--- app/src/main/java/miinjung/study/qquadet/TimeListActivity.kt
+++ app/src/main/java/miinjung/study/qquadet/TimeListActivity.kt
@@ -8,8 +8,8 @@ import androidx.recyclerview.widget.RecyclerView
 import miinjung.study.qquadet.util.backPress
 
 class TimeListActivity : AppCompatActivity() {
-    val dataList1 = arrayListOf("11:00","12:00","13:00","14:00")
-    var dataList2 = arrayListOf("1/11","2/11","3/11","4/11")
+    val dataList1 = arrayListOf("11:30 am","01:30 pm","01:40 pm","02:00 pm","02:20 pm","02:25 pm","02:40 pm","03:00 pm","03:10 pm","03:20 pm","03:30 pm","03:40 pm","03:50 pm","03:55 pm","04:00 pm","04:05 pm","04:10 pm","04:20 pm","04:40 pm")
+    var dataList2 = arrayListOf("0/5","0/7","1/4","2/8","0/3","1/3","2/6","1/4","5/6","0/3","0/7","3/8","1/8","0/9","4/5","1/9","2/3","1/11","4/10")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time_list)
